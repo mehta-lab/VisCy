a625caa647df20eca431a762b714d381337af75f
AnithaPriya Krishnan <anithapriya@gmail.com>
2018-10-19 15:51:35 -0700

    Tests regression nets (#47)
    * added tests for Image2DToVectorNet
    
    * added tests for Image3DToVectorNet

--- micro_dl/networks/base_image_to_vector_net.py
+++ micro_dl/networks/base_image_to_vector_net.py
@@ -2,13 +2,13 @@
 import numpy as np
 import tensorflow as tf
 import warnings
-from keras.layers import Activation, BatchNormalization, Dense, \
-    Dropout, Input, Flatten
+from keras.layers import BatchNormalization, Conv2D, Conv3D, Dense, Dropout, \
+    Input, Flatten
 import keras.regularizers as k_regularizers
 
 from micro_dl.networks.base_conv_net import BaseConvNet
 from micro_dl.networks.conv_blocks import conv_block,\
-    residual_downsample_conv_block
+    residual_conv_block, residual_downsample_conv_block
 from micro_dl.utils.aux_utils import validate_config
 from micro_dl.utils.network_utils import get_keras_layer, \
     create_activation_layer
@@ -17,7 +17,7 @@ from micro_dl.utils.network_utils import get_keras_layer, \
 class BaseImageToVectorNet(BaseConvNet):
     """Network for regression/classification from a set of images"""
 
-    def __init__(self, network_config):
+    def __init__(self, network_config, predict=False):
         """Init
 
         This network resembles the down-sampling half of a (res-)Unet rather
@@ -33,26 +33,52 @@ class BaseImageToVectorNet(BaseConvNet):
 
         req_params = ['num_convs_per_block',
                       'residual',
-                      'block_sequence',
-                      'regression_length']
+                      'block_sequence']
+        req_dense_params = ['type', 'regression_length']
 
-        param_check, msg = validate_config(network_config, req_params)
-        if not param_check:
-            raise ValueError(msg)
+        if not predict:
+            param_check, msg = validate_config(network_config, req_params)
+            if not param_check:
+                raise ValueError(msg)
+            dense_param_check, msg = validate_config(network_config['dense'],
+                                                     req_dense_params)
+            if not dense_param_check:
+                raise ValueError(msg)
+
+            assert network_config['dense']['type'] in ['dense', 'conv'], \
+                'Dense layers implemented as dense or 1x1 convs'
+
+            if network_config['pooling_type'] == 'conv':
+                assert 'res_pool_type' in network_config, \
+                    'pooling type for the res connection is not provided'
+
+        if network_config['dense']['type'] == 'conv':
+            network_config['dense']['dropout'] = network_config['dropout']
         self.config = network_config
 
-        max_num_conv_blocks = int(np.log2(self.config['height'])) + 1
+        max_num_conv_blocks = int(np.log2(self.config['height']))
         self.max_num_conv_blocks = max_num_conv_blocks
 
-        if 'num_initial_filters' in self.config:
-            assert 'num_filters_per_block' not in self.config, \
-                'Both num_initial_filters & num_filters_per_block provided'
-            warnings.warn('Assuming the network receptive field is the entire '
-                          'image. If not, provide num_filters_per_block'
-                          'instead of num_initial_filters', Warning)
+        if 'depth' in self.config and self.config['depth'] > 1:
+            self.config['num_dims'] = 3
+            self.conv = Conv3D
+        else:
+            self.config['num_dims'] = 2
+            self.conv = Conv2D
 
-            assert network_config['width'] == network_config['height'], \
-                'Expecting a square image for receptive field = full image'
+        if 'num_initial_filters' in self.config:
+            msg = 'Input image dimensions has to be in powers of 2 as the' \
+                  'receptive field is the entire image'
+            if not predict:
+                assert 'num_filters_per_block' not in self.config, \
+                    'Both num_initial_filters & num_filters_per_block provided'
+                assert network_config['width'] == network_config['height'], \
+                    'Expecting a square image for receptive field = full image'
+                assert (np.mod(np.log2(network_config['width']), 1) <= 0 and
+                        np.mod(np.log2(network_config['height']), 1) <= 0), msg
+                warnings.warn('Assuming the network receptive field is the '
+                              'entire image. If not, provide num_filters_per_'
+                              'block instead of num_initial_filters', Warning)
 
             num_init_filters = self.config['num_initial_filters']
             num_filters_per_block = (
@@ -60,26 +86,26 @@ class BaseImageToVectorNet(BaseConvNet):
                  for block_idx in range(max_num_conv_blocks)]
             )
             num_conv_blocks = max_num_conv_blocks
+
         elif 'num_filters_per_block' in self.config:
             num_filters_per_block = self.config['num_filters_per_block']
-            assert len(num_filters_per_block) <= max_num_conv_blocks, \
-                'network depth is incompatible with the input size'
+            if not predict:
+                assert len(num_filters_per_block) <= max_num_conv_blocks, \
+                    'network depth is incompatible with the input size'
+                assert 'num_units' in self.config['dense'], \
+                    'num_units for dense layers not provided'
             num_conv_blocks = len(num_filters_per_block)
+
         else:
             raise ValueError('Both num_initial_filters and '
                              'num_filters_per_block not in network_config')
         self.config['num_filters_per_block'] = num_filters_per_block
         self.num_conv_blocks = num_conv_blocks
 
-        if 'depth' in self.config and self.config['depth'] > 1:
-            self.config['num_dims'] = 3
-        else:
-            self.config['num_dims'] = 2
-
     def _get_regularizer_instance(self):
         """Get kernel/activity regularizer"""
 
-        kernel_reg_dict = self.config['kernel_regularizer']
+        kernel_reg_dict = self.config['dense']['kernel_regularizer']
         kernel_reg_object = getattr(k_regularizers, kernel_reg_dict['type'])
         if 'lambda' in kernel_reg_dict:
             kernel_reg_inst = kernel_reg_object(kernel_reg_dict['lambda'])
@@ -112,33 +138,43 @@ class BaseImageToVectorNet(BaseConvNet):
             block_name = 'conv_block_{}'.format(block_idx + 1)
             with tf.name_scope(block_name):
                 if self.config['residual']:
-                    layer = residual_downsample_conv_block(
-                        layer=input_layer,
-                        network_config=self.config,
-                        block_idx=block_idx,
-                    )
-                    if block_idx == self.num_conv_blocks:
-                        layer = self._downsample_layer(layer)
-
+                    if self.config['pooling_type'] == 'conv':
+                        self.config['pooling_type'] = \
+                            self.config['res_pool_type']
+                        layer = residual_downsample_conv_block(
+                            layer=input_layer,
+                            network_config=self.config,
+                            block_idx=block_idx,
+                        )
+                        if block_idx == self.num_conv_blocks:
+                            layer = self._downsample_layer(layer)
+                        self.config['pooling_type'] = 'conv'
+                    else:
+                        layer = residual_conv_block(layer=input_layer,
+                                                    network_config=self.config,
+                                                    block_idx=block_idx)
+                        if block_idx < self.num_conv_blocks:
+                             layer = self._downsample_layer(layer)
                 else:
                     layer = conv_block(layer=input_layer,
                                        network_config=self.config,
                                        block_idx=block_idx)
-                    layer = self._downsample_layer(layer)
+                    if block_idx < self.num_conv_blocks:
+                        layer = self._downsample_layer(layer)
             input_layer = layer
 
         # ----------------------- dense blocks ------------------------
         num_units = input_layer.shape.as_list()[1:]
         num_units = np.prod(num_units)
-        regression_length = self.config['regression_length']
+        regression_length = self.config['dense']['regression_length']
 
-        if 'num_dense_units' in self.config and \
-                len(self.config['num_dense_units']) > 1:
-            dense_units = self.config['num_dense_units']
+        if 'num_units' in self.config['dense'] and \
+                len(self.config['dense']['num_units']) > 1:
+            dense_units = self.config['dense']['num_units']
         else:
             if self.num_conv_blocks == self.max_num_conv_blocks:
                 if num_units / 16 > regression_length:
-                    dense_units = np.array([num_units / 2, num_units / 4,
+                    dense_units = np.array([num_units / 4,
                                             num_units / 8, num_units / 16],
                                            dtype='int')
                 elif num_units / 8 > regression_length:
@@ -154,35 +190,58 @@ class BaseImageToVectorNet(BaseConvNet):
             else:
                 raise ValueError('Invalid num_dense_units in config')
 
-        prev_dense_layer = Flatten()(layer)
+        prev_dense_layer = layer
+        if self.config['dense']['type'] == 'dense':
+            prev_dense_layer = Flatten()(layer)
+
         for dense_idx in range(len(dense_units)):
             block_name = 'dense_{}'.format(dense_idx + 1)
-            if 'kernel_regularizer' in self.config:
+            if 'kernel_regularizer' in self.config['dense']:
                 kernel_reg_inst = self._get_regularizer_instance()
             else:
                 kernel_reg_inst = None
             # can't choose b/w dropout and regularization, use both ¯\_(ツ)_/¯
             with tf.name_scope(block_name):
-                layer = Dense(
-                    units=dense_units[dense_idx],
-                    kernel_initializer=self.config['init'],
-                    kernel_regularizer=kernel_reg_inst,
-                )(prev_dense_layer)
-                layer = BatchNormalization()(layer)
+                if self.config['dense']['type'] == 'dense':
+                    layer = Dense(
+                        units=dense_units[dense_idx],
+                        kernel_initializer=self.config['init'],
+                        kernel_regularizer=kernel_reg_inst
+                    )(prev_dense_layer)
+                else:
+                    kernel_shape = (1, ) * self.config['num_dims']
+                    layer = self.conv(
+                        filters=dense_units[dense_idx],
+                        kernel_size=kernel_shape,
+                        padding='same',
+                        kernel_initializer='he_normal',
+                        data_format=self.config['data_format']
+                    )(prev_dense_layer)
 
+                layer = BatchNormalization()(layer)
                 activation_layer_instance = create_activation_layer(
                     self.config['activation']
                 )
                 layer = activation_layer_instance(layer)
-                if 'dropout_dense' in self.config:
-                    layer = Dropout(self.config['dropout_dense'])(layer)
+                if 'dropout' in self.config['dense']:
+                    layer = Dropout(self.config['dense']['dropout'])(layer)
             prev_dense_layer = layer
 
         # --------------------- output block -------------------------
         final_activation = self.config['final_activation']
         with tf.name_scope('output'):
-            outputs = Dense(regression_length,
-                            kernel_initializer='he_normal',
-                            activation=final_activation)(prev_dense_layer)
-
+            if self.config['dense']['type'] == 'dense':
+                outputs = Dense(regression_length,
+                                kernel_initializer='he_normal',
+                                activation=final_activation)(prev_dense_layer)
+            else:
+                kernel_shape = (1,) * self.config['num_dims']
+                outputs = self.conv(
+                    filters=regression_length,
+                    padding='same',
+                    kernel_size=kernel_shape,
+                    kernel_initializer='he_normal',
+                    data_format=self.config['data_format']
+                )(prev_dense_layer)
+                outputs = Flatten()(outputs)
         return inputs, outputs
---
+++ tests/networks/image2D_to_vector_net_tests.py
@@ -0,0 +1,207 @@
+"""Tests for Image2DToVectorNet"""
+
+from keras import Model
+import nose.tools
+import numpy as np
+import unittest
+
+from micro_dl.networks import Image2DToVectorNet
+
+
+class TestImage2DToVectorNet(unittest.TestCase):
+
+    def setUp(self):
+        """Set up network_config, model and model layers"""
+
+        self.network_config = {'num_input_channels': 4,
+                               'data_format': 'channels_first',
+                               'height': 64,
+                               'width': 64,
+                               'batch_norm': True,
+                               'dropout': 0.2,
+                               'pooling_type': 'average',
+                               'block_sequence': 'conv-bn-activation',
+                               'num_initial_filters': 16,
+                               'num_convs_per_block': 2,
+                               'residual': True,
+                               'dense': {
+                                   'type': 'dense',
+                                   'dropout': 0.5,
+                                   'regression_length': 7,
+                                   'kernel_regularizer': {
+                                       'type': 'l2',
+                                       'lambda': 0.001
+                                   },
+                               },
+                               'final_activation': 'linear'}
+
+        self.net = Image2DToVectorNet(self.network_config)
+        inputs, outputs = self.net.build_net()
+        self.model = Model(inputs, outputs)
+        self.model_layers = self.model.layers
+
+    def test_Image2DToVectorNet_init(self):
+        """Test initialization"""
+
+        # check if both num_inital_filters and num_filters_per_block provided
+        self.network_config['num_filters_per_block'] = [16, 32, 48, 64]
+        nose.tools.assert_raises(AssertionError,
+                                 Image2DToVectorNet,
+                                 self.network_config)
+        self.network_config.pop('num_filters_per_block')
+
+        # check for dense_type
+        self.network_config['dense']['type'] = 'pool'
+        nose.tools.assert_raises(AssertionError,
+                                 Image2DToVectorNet,
+                                 self.network_config)
+        self.network_config['dense']['type'] = 'dense'
+
+        # check for drop_out with 1x1 conv, res_pool_type
+        self.network_config['pooling_type'] = 'conv'
+        nose.tools.assert_raises(AssertionError,
+                                 Image2DToVectorNet,
+                                 self.network_config)
+        self.network_config['dense']['type'] = 'dense'
+
+        # check for height and width not being powers of 2
+        self.network_config['width'] = 48
+        nose.tools.assert_raises(AssertionError,
+                                 Image2DToVectorNet,
+                                 self.network_config)
+        self.network_config['width'] = 64
+
+    def test_Image2DToVectorNet_get_input_shape(self):
+        """Test input and output shape of network"""
+
+        exp_in_shape = (self.network_config['num_input_channels'],
+                        self.network_config['height'],
+                        self.network_config['width'])
+
+        exp_out_shape = (self.network_config['dense']['regression_length'], )
+        in_shape = self.model_layers[0].output_shape[1:]
+        out_shape = self.model_layers[-1].output_shape[1:]
+        nose.tools.assert_equal(in_shape, exp_in_shape)
+        nose.tools.assert_equal(out_shape, exp_out_shape)
+
+    def test_Image2DToVectorNet_init_filters_res_shapes(self):
+        """Test for intermediate shapes"""
+
+        conv_idx = [0, 4]
+        cur_feature_shape = [self.network_config['num_input_channels'],
+                             self.network_config['height'],
+                             self.network_config['width']]
+        down_block_idx = [[1, 12], [12, 23], [23, 34],
+                          [34, 45], [45, 56], [56, 67]]
+        dense_block_idx = np.array([68, 72, 76, 80])
+        dense_units = [128, 64, 32, 7]
+
+        for idx, down_idx in enumerate(down_block_idx):
+            cur_down_block = self.model_layers[down_idx[0]: down_idx[1]]
+            cur_feature_shape[0] = \
+                self.net.config['num_filters_per_block'][idx]
+            nose.tools.assert_equal(
+                cur_down_block[conv_idx[0]].output_shape[1:],
+                tuple(cur_feature_shape)
+            )
+            nose.tools.assert_equal(
+                cur_down_block[conv_idx[1]].output_shape[1:],
+                tuple(cur_feature_shape)
+            )
+
+            # res add
+            nose.tools.assert_equal(cur_down_block[-2].output_shape[1:],
+                                    tuple(cur_feature_shape))
+
+            if idx < 6:
+                cur_feature_shape[1] = cur_feature_shape[1] // 2
+                cur_feature_shape[2] = cur_feature_shape[2] // 2
+
+                pool_feature_shape = (
+                    self.net.config['num_filters_per_block'][idx],
+                    cur_feature_shape[1],
+                    cur_feature_shape[2]
+                )
+                # pool layer
+                nose.tools.assert_equal(cur_down_block[-1].output_shape[1:],
+                                        pool_feature_shape)
+
+        for dense_type in ['dense', 'conv']:
+            if dense_type == 'conv':
+                dense_block_idx = dense_block_idx - 1
+                self.network_config['dense']['type'] = 'conv'
+                self.network_config.pop('num_filters_per_block')
+                self.net = Image2DToVectorNet(self.network_config)
+                inputs, outputs = self.net.build_net()
+                self.model = Model(inputs, outputs)
+                self.model_layers = self.model.layers
+
+            for idx, dense_idx in enumerate(dense_block_idx):
+                cur_dense_block = self.model_layers[dense_idx]
+                nose.tools.assert_equal(cur_dense_block.output_shape[1],
+                                        dense_units[idx])
+
+    def test_Image2DToVectorNet_filters_per_block_shapes(self):
+        """Test for intermediate shapes"""
+
+        self.network_config.pop('num_initial_filters')
+        self.network_config['num_filters_per_block'] = [8, 16, 32, 48]
+        self.network_config['pooling_type'] = 'conv'
+        self.network_config['res_pool_type'] = 'average'
+        self.network_config['dense']['num_units'] = [32, 16]
+        self.network_config['dense']['type'] = 'conv'
+        self.net = Image2DToVectorNet(self.network_config)
+        inputs, outputs = self.net.build_net()
+        self.model = Model(inputs, outputs)
+        self.model_layers = self.model.layers
+
+        conv_idx = [0, 4]
+        cur_feature_shape = [self.network_config['num_input_channels'],
+                             self.network_config['height'],
+                             self.network_config['width']]
+        down_block_idx = [[1, 11], [11, 22], [22, 33], [33, 44]]
+        dense_block_idx = np.array([44, 48, 52])
+        dense_units = [32, 16, 7]
+
+        for idx, down_idx in enumerate(down_block_idx):
+            # conv layers
+            cur_down_block = self.model_layers[down_idx[0]: down_idx[1]]
+            cur_feature_shape[0] = \
+                self.network_config['num_filters_per_block'][idx]
+
+            if idx > 0:
+                cur_feature_shape[1] = cur_feature_shape[1] // 2
+                cur_feature_shape[2] = cur_feature_shape[2] // 2
+                pool_feature_shape = (
+                    self.network_config['num_filters_per_block'][idx - 1],
+                    cur_feature_shape[1],
+                    cur_feature_shape[2]
+                )
+                # pool layer
+                nose.tools.assert_equal(
+                    cur_down_block[7].output_shape[1:],
+                    pool_feature_shape
+                )
+
+            nose.tools.assert_equal(
+                cur_down_block[conv_idx[0]].output_shape[1:],
+                tuple(cur_feature_shape)
+            )
+            nose.tools.assert_equal(
+                cur_down_block[conv_idx[1]].output_shape[1:],
+                tuple(cur_feature_shape)
+            )
+            # res add
+            nose.tools.assert_equal(cur_down_block[-1].output_shape[1:],
+                                    tuple(cur_feature_shape))
+
+        for idx, dense_idx in enumerate(dense_block_idx):
+            cur_dense_block = self.model_layers[dense_idx]
+            nose.tools.assert_equal(cur_dense_block.output_shape[1:],
+                                    (dense_units[idx],
+                                     cur_feature_shape[1],
+                                     cur_feature_shape[2]))
+
+    # TODO: fix the test for all weights getting trained. The weights
+    # don't seem to budge when trying to predict random vectors or vector of
+    # ones at various learning rates and lambdas
