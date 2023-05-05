3523909c887ff4da4fd7a3126947c28312b72254
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-05-05 09:26:37 -0700

    save to config root dir by default (#230)

--- micro_dl/light/train.py
+++ micro_dl/light/train.py
@@ -15,12 +15,13 @@ class VSLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         # https://pytorch-lightning.readthedocs.io/en/1.6.0/api/pytorch_lightning.utilities.cli.html#pytorch_lightning.utilities.cli.LightningCLI.add_arguments_to_parser
         parser.link_arguments("data.batch_size", "model.batch_size")
+        parser.link_arguments("trainer.default_root_dir","trainer.logger.init_args.save_dir")
         parser.add_argument("--architecture", type=str, default="2.5D")
         parser.set_defaults(
             {
                 "trainer.logger": lazy_instance(
                     TensorBoardLogger,
-                    save_dir=os.path.expanduser('~'),  # link_arguments("trainer.default_root_dir","trainer.logger.save_dir") didn't work.
+                    save_dir="",
                     version=datetime.now().strftime(r"%Y%m%d-%H%M%S"),
                     log_graph=True,
                 )
