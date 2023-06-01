e65184f4be26d0e3267e06fed4f76b441ec73b71
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2023-06-01 11:33:02 -0700

    remove tensorflow environment files

--- setup_env_DLMBL21.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# create an environment - can take time, should be run at the start of image translation exercise.
-conda env create --file=conda_environment.yml
-conda init bash
-conda activate micro_dl
-export PYTHONPATH=$PYTHONPATH:$(pwd)
