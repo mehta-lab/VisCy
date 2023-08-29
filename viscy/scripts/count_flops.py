f6fdc0834f661fe8b43e8a137c8b35b48d97e057
Ziwen Liu <ziwen.liu@czbiohub.org>
2023-08-28 20:53:15 -0700

    upgrade flop counting

--- viscy/scripts/count_flops.py
+++ viscy/scripts/count_flops.py
@@ -6,20 +6,21 @@ from viscy.light.engine import VSUNet
 
 # %%
 model = VSUNet(
+    architecture="2.2D",
     model_config={
-        "architecture": "2.1D",
         "in_channels": 1,
         "out_channels": 2,
-        "in_stack_depth": 9,
+        "in_stack_depth": 5,
         "backbone": "convnextv2_tiny",
-        "stem_kernel_size": (9, 4, 4),
+        "stem_kernel_size": (5, 4, 4),
     },
 )
 
 # %%
 with torch.cuda.device(0):
     macs, params = get_model_complexity_info(
-        model, (1, 9, 2048, 2048), print_per_layer_stat=False
+        model,
+        (1, 5, 2048, 2048),  # print_per_layer_stat=False
     )
 print(macs, params)
 # %%
