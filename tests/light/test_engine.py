72de113f8c5a678f4383d374da925517d1cace6b
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-02-23 22:33:41 -0800

    update tests

--- tests/light/test_engine.py
+++ tests/light/test_engine.py
@@ -3,8 +3,5 @@ from viscy.light.engine import FcmaeUNet
 
 def test_fcmae_vsunet() -> None:
     model = FcmaeUNet(
-        architecture="fcmae",
-        model_config=dict(in_channels=3),
-        train_mask_ratio=0.6,
+        model_config=dict(in_channels=3, out_channels=1), fit_mask_ratio=0.6
     )
-
