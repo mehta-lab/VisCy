5fc9da2756285dd7bcc35f576910e873e1e9bdbf
Soorya Pradeep <soorya.pradeep@czbiohub.org>
2024-03-04 19:35:06 -0800

    chnaged to simple unet model

--- examples/infection_phenotyping/Infection_classification_model.py
+++ examples/infection_phenotyping/Infection_classification_model.py
@@ -1,25 +1,24 @@
 
 # %%
 import torch
-import sys
 from viscy.data.hcs import HCSDataModule
-# from lightning.pytorch import CustomDataset
-from lightning.pytorch.callbacks import Callback
-from lightning.pytorch import LightningDataModule
-# import cv2
+
 import numpy as np
 import torch.nn as nn
-import torchvision.models as models
 import lightning.pytorch as pl
 import torch.nn.functional as F
-from viscy.light.engine import VSUNet
+
+import napari
+from pytorch_lightning.loggers import TensorBoardLogger
+from monai.transforms import Zoom
+from monai.transforms import Compose, RandRotate, Resize, Zoom, Flip, RandFlip, RandZoom, RandRotate90, RandRotate, RandAffine, Rand2DElastic, Rand3DElastic, RandGaussianNoise, RandGaussianNoised
 
 # %% Create a dataloader and visualize the batches.
 # Set the path to the dataset
 dataset_path = "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/datasets/Exp_2023_09_28_DENV_A2_infMarked.zarr"
 
 # Create an instance of HCSDataModule
-data_module = HCSDataModule(dataset_path, source_channel=['Sensor','Nucl_mask'], target_channel=['Inf_mask'],yx_patch_size=[256, 256], split_ratio=0.8, z_window_size=1, architecture = '2D')
+data_module = HCSDataModule(dataset_path, source_channel=['Sensor','Nucl_mask'], target_channel=['Inf_mask'],yx_patch_size=[128,128], split_ratio=0.8, z_window_size=1, architecture = '2D')
 
 # Prepare the data
 data_module.prepare_data()
@@ -31,10 +30,6 @@ data_module.setup(stage = "fit")
 dataloader = data_module.train_dataloader()
 
 # Visualize the dataset and the batch using napari
-import napari
-from pytorch_lightning.loggers import TensorBoardLogger
-# import os
-
 # Set the display
 # os.environ['DISPLAY'] = ':1'
 
@@ -73,23 +68,86 @@ def dice_loss(pred, target):
     
     return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth) )
 
-unet_model = VSUNet(
-    architecture='2D', 
-    loss_function=dice_loss, 
-    lr=1e-3, 
-    example_input_xy_shape=(64,64)
-    )
+# load 2D UNet from viscy
+# unet_model = VSUNet(
+#     architecture='2D', 
+#     model_config={"in_channels": 2, "out_channels": 1},
+#     loss_function=dice_loss, 
+#     lr=1e-3, 
+#     example_input_xy_shape=(128,128),
+#     )
+
+# Define the data augmentations
+# Define the augmentations
+# transforms = Compose([
+#     RandRotate(range_x=15, prob=0.5),
+#     Resize(spatial_size=[64, 64],mode='linear'),
+#     Zoom([0.5,2], mode='bilinear'),
+#     Flip(spatial_axis=[0,1]),
+#     RandFlip(spatial_axis=[0,1], prob=0.5),
+#     RandZoom(min_zoom=0.9, max_zoom=1.1, prob=0.5),
+#     RandRotate90(spatial_axes=(0,1), prob=0.2, max_k=3),
+#     RandGaussianNoise(prob=0.5),
+# ])
+
+transforms = Compose([
+    Flip(spatial_axis=[0,1]),
+])
+
+# create a small unet for image translation which accepts two input images (a label image and a microscopy image) and outputs one label image
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+
+        self.encoder = nn.Sequential(
+            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
+        )
+
+        # Define the decoder part of the U-Net architecture
+        self.decoder = nn.Sequential(
+            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(64, out_channels, kernel_size=1)
+        )
+
+    def forward(self, x):
+        # Apply the encoder to the input
+        x = self.encoder(x)
+
+        # Apply the decoder to the output of the encoder
+        x = self.decoder(x)
+
+        return x
+
+unet_model = UNet(in_channels=2, out_channels=1)
 
 # Define the optimizer
 optimizer = torch.optim.Adam(unet_model.parameters(), lr=1e-3)
 
-# Iterate over the batches
+#%% Iterate over the batches
 for batch in dataloader:
     # Extract the input and target from the batch
     input_data, target = batch['source'], batch['target']
 
+    # Apply the augmentations to your data
+    augmented_input = transforms(input_data)
+
     # Forward pass through the model
-    output = unet_model(input_data)
+    output = unet_model(augmented_input,target)
 
     # Apply softmax activation to the output
     output = F.softmax(output, dim=1)
@@ -101,6 +159,11 @@ for batch in dataloader:
     loss.backward()
     optimizer.step()
     optimizer.zero_grad()
+
+# Visualize sample of the augmented data using napari
+for i in range(augmented_data.shape[0]):
+    viewer.add_image(augmented_data[i].cpu().numpy().astype(np.float32))
+
 
 #%% use the batch for training the unet model using the lightning module
     
@@ -139,4 +202,5 @@ for batch in test_dataloader:
 predictions = np.stack(predictions)
 
 # Save the predictions as added channel in zarr format
+# use iohub or viscy to save the predictions!!!
 zarr.save('predictions.zarr', predictions)
