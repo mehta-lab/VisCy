34b81b952f34c8a00e190e354aa544b51fee5f82
Soorya Pradeep <soorya.pradeep@czbiohub.org>
2024-03-06 15:56:07 -0800

    added tensorboard logging

--- examples/infection_phenotyping/Infection_classification_model.py
+++ examples/infection_phenotyping/Infection_classification_model.py
@@ -10,14 +10,26 @@ import torch.nn.functional as F
 
 import napari
 from pytorch_lightning.loggers import TensorBoardLogger
-from monai.transforms import Compose, RandRotate, Resize, Zoom, Flip, RandFlip, RandZoom, RandRotate90, RandRotate, RandAffine, Rand2DElastic, Rand3DElastic, RandGaussianNoise, RandGaussianNoised
+from monai.transforms import RandRotate, Resize, Zoom, Flip, RandFlip, RandZoom, RandRotate90, RandRotate, RandAffine, Rand2DElastic, Rand3DElastic, RandGaussianNoise, RandGaussianNoised
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 # %% Create a dataloader and visualize the batches.
 # Set the path to the dataset
 dataset_path = "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/datasets/Exp_2023_09_28_DENV_A2_infMarked.zarr"
 
 # Create an instance of HCSDataModule
-data_module = HCSDataModule(dataset_path, source_channel=['Sensor','Nucl_mask'], target_channel=['Inf_mask'],yx_patch_size=[128,128], split_ratio=0.8, z_window_size=1, architecture = '2D')
+data_module = HCSDataModule(
+    dataset_path, 
+    source_channel=['Sensor','Nucl_mask'], 
+    target_channel=['Inf_mask'],
+    yx_patch_size=[128,128], 
+    split_ratio=0.8, 
+    z_window_size=1, 
+    architecture = '2D',
+    num_workers=1,
+    batch_size=12,
+    augmentations=[],
+)
 
 # Prepare the data
 data_module.prepare_data()
@@ -32,41 +44,41 @@ dataloader = data_module.train_dataloader()
 # Set the display
 # os.environ['DISPLAY'] = ':1'
 
-# Create a napari viewer
-viewer = napari.Viewer()
+# # Create a napari viewer
+# viewer = napari.Viewer()
 
-# Add the dataset to the viewer
-for batch in dataloader:
-    if isinstance(batch, dict):
-        for k, v in batch.items():
-            if isinstance(v, torch.Tensor):
-                viewer.add_image(v.cpu().numpy().astype(np.float32))
+# # Add the dataset to the viewer
+# for batch in dataloader:
+#     if isinstance(batch, dict):
+#         for k, v in batch.items():
+#             if isinstance(v, torch.Tensor):
+#                 viewer.add_image(v.cpu().numpy().astype(np.float32))
 
-# Start the napari event loop
-napari.run()
+# # Start the napari event loop
+# napari.run()
 
 # %% use 2D Unet from viscy with a softmax layer at end for 4 label classification
 # use for image translation from instance segmentation to annotated image
 
-# use diceloss function from here: https://gist.github.com/weiliu620/52d140b22685cf9552da4899e2160183
-def dice_loss(pred, target):
-    """This definition generalize to real valued pred and target vector. This should be differentiable.
-    pred: tensor with first dimension as batch
-    target: tensor with first dimension as batch
-    """
+# use diceloss function from here: https://www.kaggle.com/code/bigironsphere/loss-function-library-keras-pytorch
+class DiceLoss(nn.Module):
+    def __init__(self, weight=None, size_average=True):
+        super(DiceLoss, self).__init__()
 
-    smooth = 1.
-
-    # have to use contiguous since they may from a torch.view op
-    iflat = pred.contiguous().view(-1)
-    tflat = target.contiguous().view(-1)
-    intersection = (iflat * tflat).sum()
-
-    A_sum = torch.sum(tflat * iflat)
-    B_sum = torch.sum(tflat * tflat)
+    def forward(self, inputs, targets, smooth=1):
+        
+        #comment out if your model contains a sigmoid or equivalent activation layer
+        inputs = F.sigmoid(inputs)       
+        
+        #flatten label and prediction tensors
+        inputs = inputs.view(-1)
+        targets = targets.view(-1)
+        
+        intersection = (inputs * targets).sum()                            
+        dice = (2.*intersection + smooth)/(inputs.sum() + targets.sum() + smooth)  
+        
+        return 1 - dice
     
-    return 1 - ((2. * intersection + smooth) / (A_sum + B_sum + smooth) )
-
 # load 2D UNet from viscy
 # unet_model = VSUNet(
 #     architecture='2D', 
@@ -76,67 +88,43 @@ def dice_loss(pred, target):
 #     example_input_xy_shape=(128,128),
 #     )
 
-# Define the data augmentations
-# Define the augmentations
-# transforms = Compose([
-#     RandRotate(range_x=15, prob=0.5),
-#     Resize(spatial_size=[64, 64],mode='linear'),
-#     Zoom([0.5,2], mode='bilinear'),
-#     Flip(spatial_axis=[0,1]),
-#     RandFlip(spatial_axis=[0,1], prob=0.5),
-#     RandZoom(min_zoom=0.9, max_zoom=1.1, prob=0.5),
-#     RandRotate90(spatial_axes=(0,1), prob=0.2, max_k=3),
-#     RandGaussianNoise(prob=0.5),
-# ])
-
-transforms = Compose([
-    RandRotate(range_x=15, prob=0.5),
-    Flip(spatial_axis=[0,1]),
-    RandFlip(spatial_axis=[0,1], prob=0.5),
-    RandRotate90(spatial_axes=(0,1), prob=0.2, max_k=3),
-    RandGaussianNoise(prob=0.5),
-])
-
 # create a small unet for image translation which accepts two input images (a label image and a microscopy image) and outputs one label image
 class UNet(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UNet, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+            nn.Conv3d(in_channels, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.MaxPool3d(kernel_size=1, stride=1),
+            nn.Conv3d(64, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.MaxPool3d(kernel_size=1, stride=1),
+            nn.Conv3d(128, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(256, 512, kernel_size=3, padding=1),
+            nn.MaxPool3d(kernel_size=1, stride=1),
+            nn.Conv3d(256, 512, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
 
-        # Define the decoder part of the U-Net architecture
         self.decoder = nn.Sequential(
-            nn.Conv2d(512, 256, kernel_size=3, padding=1),
+            nn.Conv3d(512, 256, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(256, 128, kernel_size=3, padding=1),
+            nn.Conv3d(256, 128, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.Conv3d(128, 64, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(128, out_channels, kernel_size=1),
+            nn.Conv3d(64, out_channels, kernel_size=1),
             nn.Softmax(dim=1),
-            nn.Conv2d(out_channels, out_channels, kernel_size=1),
+            nn.Conv3d(out_channels, out_channels, kernel_size=1),
             nn.Softmax(dim=1),
-            nn.Conv2d(out_channels, out_channels, kernel_size=1),
+            nn.Conv3d(out_channels, out_channels, kernel_size=1),
             nn.Softmax(dim=1)
         )
 
     def forward(self, x):
-        # Apply the encoder to the input
         x = self.encoder(x)
 
-        # Apply the decoder to the output of the encoder
         x = self.decoder(x)
 
         return x
@@ -150,19 +138,13 @@ optimizer = torch.optim.Adam(unet_model.parameters(), lr=1e-3)
 for batch in dataloader:
     # Extract the input and target from the batch
     input_data, target = batch['source'], batch['target']
-
-    # Apply the augmentations to your data
-    augmented_input = transforms(input_data)
-    viewer.add_image(augmented_input.cpu().numpy().astype(np.float32))
+    # viewer.add_image(input_data.cpu().numpy().astype(np.float32))
 
     # Forward pass through the model
-    output = unet_model(augmented_input)
-
-    # Apply softmax activation to the output
-    output = F.softmax(output, dim=1)
+    output = unet_model(input_data)
 
     # Calculate the loss
-    loss = dice_loss(output, target)
+    loss = DiceLoss()(output, target)
 
     # Perform backpropagation and update the model's parameters
     loss.backward()
@@ -178,10 +160,46 @@ for batch in dataloader:
     
 # Train the model
 # Create a TensorBoard logger
-logger = TensorBoardLogger("logs", name="infection_classification_model")
+class LightningUNet(pl.LightningModule):
+    def __init__(self, in_channels, out_channels):
+        super(LightningUNet, self).__init__()
+        self.unet_model = UNet(in_channels, out_channels)
+
+    def forward(self, x):
+        return self.unet_model(x)
+
+    def training_step(self, batch, batch_idx):
+        input_data, target = batch['source'], batch['target']
+        output = self(input_data)
+        loss = DiceLoss()(output, target)
+        self.log('train_loss', loss)
+        return loss
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.parameters(), lr=1e-3)
+        return optimizer
+
+# Create an instance of the LightningUNet class
+unet_model = LightningUNet(in_channels=2, out_channels=1)
+
+# Define the logger
+logger = TensorBoardLogger("/hpc/projects/comp.micro/infected_cell_imaging/Single_cell_phenotyping/Infection_phenotyping_data/logs", name="infection_classification_model")
 
 # Pass the logger to the Trainer
-trainer = pl.Trainer(gpus=1, logger=logger)
+trainer = pl.Trainer(logger=logger, max_epochs=10, default_root_dir="/hpc/projects/comp.micro/infected_cell_imaging/Single_cell_phenotyping/Infection_phenotyping_data/logs", log_every_n_steps=1)
+
+# Define the checkpoint callback
+checkpoint_callback = ModelCheckpoint(
+    dirpath='/hpc/projects/comp.micro/infected_cell_imaging/Single_cell_phenotyping/Infection_phenotyping_data/checkpoints',
+    filename='checkpoint_{epoch:02d}',
+    save_top_k=-1,
+    verbose=True,
+    monitor='val_loss',
+    mode='min'
+)
+
+# Add the checkpoint callback to the trainer
+trainer.callbacks.append(checkpoint_callback)
 
 # Fit the model
 trainer.fit(unet_model, data_module)
