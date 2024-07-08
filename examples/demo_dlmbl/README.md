add21bfaee6aec7bfc64847f39c1d553665d190e
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2024-07-08 09:54:31 -0700

    updating the readme and setup.sh

--- examples/demo_dlmbl/README.md
+++ examples/demo_dlmbl/README.md
@@ -1,6 +1,6 @@
 # Exercise 6: Image translation - Part 1
 
-This demo script was developed for the DL@MBL 2023 course by Eduardo Hirata-Miyasaki, Ziwen Liu and Shalin Mehta, with many inputs and bugfixes by [Morgan Schwartz](https://github.com/msschwartz21), [Caroline Malin-Mayor](https://github.com/cmalinmayor), and [Peter Park](https://github.com/peterhpark).
+This demo script was developed for the DL@MBL 2024 course by Eduardo Hirata-Miyasaki, Ziwen Liu and Shalin Mehta, with many inputs and bugfixes by [Morgan Schwartz](https://github.com/msschwartz21), [Caroline Malin-Mayor](https://github.com/cmalinmayor), and [Peter Park](https://github.com/peterhpark).  
 
 
 
@@ -42,7 +42,8 @@ jupyter notebook
 
 ...and continue with the instructions in the notebook.
 
-If 06_image_translation is not available as a kernel in jupyter, run
+If `06_image_translation` is not available as a kernel in jupyter, run:
+
 ```
 python -m ipykernel install --user --name=06_image_translation
 ```
--- examples/demo_dlmbl/setup.sh
+++ examples/demo_dlmbl/setup.sh
@@ -15,7 +15,8 @@ mkdir -p ~/code/
 cd ~/code/
 git clone https://github.com/mehta-lab/viscy.git
 cd viscy
-git checkout main # Exercise is tested with this commit of viscy
+git checkout main #FIXME: change after merging this PR   # Exercise is tested with this commit of viscy
+
 # Find path to the environment - conda activate doesn't work from within shell scripts.
 ENV_PATH=$(conda info --envs | grep 06_image_translation | awk '{print $NF}')
 $ENV_PATH/bin/pip install ".[metrics]"
@@ -26,11 +27,10 @@ mkdir -p ~/data/06_image_translation/test
 
 # Change to the target directory
 cd ~/data/06_image_translation/training
-# Download the Zarr dataset recursively (if the server supports it)
-wget -m -np -nH --cut-dirs=4 -R "index.html*" "https://public.czbiohub.org/comp.micro/viscy/VSCyto2D/training/a549_hoechst_cellmask_train_val.zarr/"
 
+# Download the OME-Zarr dataset recursively
+wget -m -np -nH --cut-dirs=4 -R "index.html*" "https://public.czbiohub.org/comp.micro/viscy/VSCyto2D/training/a549_hoechst_cellmask_train_val.zarr/"
 cd ~/data/06_image_translation/test
-# Download the Zarr dataset recursively (if the server supports it)
 wget -m -np -nH --cut-dirs=4 -R "index.html*" "https://public.czbiohub.org/comp.micro/viscy/VSCyto2D/test/a549_hoechst_cellmask_test.zarr/"
 
 # Change back to the starting directory
