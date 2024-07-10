173a5dbce68214d2ffbdbb11873a68f5445cf3e7
Ziwen Liu <67518483+ziw-liu@users.noreply.github.com>
2024-07-10 15:11:59 -0700

    fix float comp

--- tests/preprocessing/test_pixel_ratio.py
+++ tests/preprocessing/test_pixel_ratio.py
@@ -1,4 +1,5 @@
 from iohub.ngff import open_ome_zarr
+from numpy.testing import assert_allclose
 
 from viscy.preprocessing.pixel_ratio import sematic_class_weights
 
@@ -6,7 +7,7 @@ from viscy.preprocessing.pixel_ratio import sematic_class_weights
 def test_sematic_class_weights(small_hcs_dataset):
     weights = sematic_class_weights(small_hcs_dataset, "GFP")
     assert weights.shape == (3,)
-    assert weights[0] == 1.0
+    assert_allclose(weights[0], 1.0)
     # infinity
     assert weights[1] > 1.0
     assert weights[2] > 1.0
