4d787f229c2c4f036c2127ed8b4f22edb6b812b5
Alishba Imran <alishbai734@gmail.com>
2024-07-29 14:36:45 -0700

    updated structure

---
+++ applications/contrastive_phenotyping/graphs_ConvNeXt_ResNet.py
@@ -0,0 +1,161 @@
+# %% Imports and paths.
+import os
+import torch
+from viscy.representation.contrastive import ContrastiveEncoder
+import torchview
+import timm
+
+# uncomment if you are using jupyter and want to autoreload the updated code.
+# %load_ext autoreload
+# %autoreload 2
+
+# %% Explore model graphs returned by timm
+
+convnextv1 = timm.create_model(
+    "convnext_tiny", pretrained=False, features_only=False, num_classes=200
+)
+print(convnextv1)
+output = convnextv1(torch.randn(1, 3, 256, 256))
+print(output.shape)
+# %% Initialize the model and log the graph: convnext.
+in_channels = 1
+in_stack_depth = 15
+
+contrastive_convnext1 = ContrastiveEncoder(
+    backbone="convnext_tiny", in_channels=in_channels, in_stack_depth=in_stack_depth
+)
+print(contrastive_convnext1)
+
+
+projections, embedding = contrastive_convnext1(
+    torch.randn(1, in_channels, in_stack_depth, 256, 256)
+)
+print(
+    f"shape of projections:{projections.shape}, shape of embedding: {embedding.shape}"
+)
+# %%
+
+in_channels = 3
+in_stack_depth = 18
+
+contrastive_convnext2 = ContrastiveEncoder(
+    backbone="convnextv2_tiny", in_channels=in_channels, in_stack_depth=in_stack_depth
+)
+print(contrastive_convnext2)
+embedding, projections = contrastive_convnext2(
+    torch.randn(1, in_channels, in_stack_depth, 256, 256)
+)
+print(
+    f"shape of projections:{projections.shape}, shape of embedding: {embedding.shape}"
+)
+
+# %%
+in_channels = 10
+in_stack_depth = 12
+contrastive_resnet = ContrastiveEncoder(
+    backbone="resnet50",
+    in_channels=in_channels,
+    in_stack_depth=in_stack_depth,
+    embedding_len=256,
+)
+print(contrastive_resnet)
+embedding, projections = contrastive_resnet(
+    torch.randn(1, in_channels, in_stack_depth, 256, 256)
+)
+print(
+    f"shape of projections:{projections.shape}, shape of embedding: {embedding.shape}"
+)
+
+# %%
+plot_model = contrastive_resnet
+model_graph = torchview.draw_graph(
+    plot_model,
+    input_size=(20, in_channels, in_stack_depth, 224, 224),
+    depth=3,  # adjust depth to zoom in.
+    device="cpu",
+)
+# Print the image of the model.
+model_graph.resize_graph(scale=2.5)
+model_graph.visual_graph
+
+# %% Initialize a resent50 model and log the graph.
+contra_model = ContrastiveEncoder(
+    backbone="resnet50", in_stack_depth=16, stem_kernel_size=(4, 3, 3)
+)  # note that the resnet first layer takes 64 channels (so we can't have multiples of 3)
+print(contra_model)
+model_graph = torchview.draw_graph(
+    contra_model,
+    torch.randn(1, 2, 16, 224, 224),
+    depth=3,  # adjust depth to zoom in.
+    device="cpu",
+)
+# Print the image of the model.
+model_graph.resize_graph(scale=2.5)
+model_graph.visual_graph
+
+
+# %% Initiatlize the lightning module and view the model.
+contrastive_module = ContrastiveModule()
+print(contrastive_module.encoder)
+
+# %%
+model_graph = torchview.draw_graph(
+    contrastive_module.encoder,
+    torch.randn(1, in_channels, in_stack_depth, 200, 200),
+    depth=3,  # adjust depth to zoom in.
+    device="cpu",
+)
+# Print the image of the model.
+model_graph.visual_graph
+
+# %% Playground
+
+available_models = timm.list_models(pretrained=True)
+
+stem = UNeXt2Stem(
+    in_channels=2, out_channels=96, kernel_size=(5, 2, 2), in_stack_depth=15
+)
+print(stem)
+stem_graph = torchview.draw_graph(
+    stem,
+    torch.randn(1, 2, 15, 256, 256),
+    depth=2,  # adjust depth to zoom in.
+    device="cpu",
+)
+# Print the image of the model.
+stem_graph.visual_graph
+# %%
+encoder = timm.create_model(
+    "convnext_tiny",
+    pretrained=True,
+    features_only=False,
+    num_classes=200,
+)
+
+print(encoder)
+
+# %%
+
+encoder.stem = stem
+
+model_graph = torchview.draw_graph(
+    encoder,
+    torch.randn(1, 2, 15, 256, 256),
+    depth=2,  # adjust depth to zoom in.
+    device="cpu",
+)
+# Print the image of the model.
+model_graph.visual_graph
+# %%
+encoder.stem = torch.nn.Identity()
+
+encoder_graph = torchview.draw_graph(
+    encoder,
+    torch.randn(1, 96, 128, 128),
+    depth=2,  # adjust depth to zoom in.
+    device="cpu",
+)
+# Print the image of the model.
+encoder_graph.visual_graph
+
+# %%
---
+++ applications/contrastive_phenotyping/training_script.py
@@ -0,0 +1,187 @@
+# %% Imports and paths.
+import logging
+import os
+from argparse import ArgumentParser
+from pathlib import Path
+
+import torch
+import torchview
+from lightning.pytorch import Trainer
+from lightning.pytorch.callbacks import (
+    ModelCheckpoint,
+)
+
+# from lightning.pytorch.loggers import TensorBoardLogger
+from lightning.pytorch.loggers import WandbLogger
+from lightning.pytorch.strategies import DDPStrategy
+
+from viscy.data.hcs import ContrastiveDataModule
+from viscy.light.engine import ContrastiveModule
+from viscy.representation.contrastive import ContrastiveEncoder
+
+# Set W&B logging level to suppress warnings
+logging.getLogger("wandb").setLevel(logging.ERROR)
+
+# %% Paths and constants
+os.environ["WANDB_DIR"] = "/hpc/mydata/alishba.imran/wandb_logs/"
+
+# @rank_zero_only
+# def init_wandb():
+#     wandb.init(project="contrastive_model", dir="/hpc/mydata/alishba.imran/wandb_logs/")
+
+# init_wandb()
+
+# wandb.init(project="contrastive_model", dir="/hpc/mydata/alishba.imran/wandb_logs/")
+
+top_dir = Path("/hpc/projects/intracellular_dashboard/viral-sensor/")
+# input_zarr = top_dir / "2024_02_04_A549_DENV_ZIKV_timelapse/6-patches/full_patch.zarr"
+input_zarr = "/hpc/projects/virtual_staining/viral_sensor_test_dataio/2024_02_04_A549_DENV_ZIKV_timelapse/6-patches/full_patch.zarr"
+model_dir = top_dir / "infection_classification/models/infection_score"
+# checkpoint dir: /hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/models/infection_score/updated_multiple_channels
+timesteps_csv_path = (
+    top_dir / "2024_02_04_A549_DENV_ZIKV_timelapse/6-patches/final_track_timesteps.csv"
+)
+
+# Data parameters
+base_path = "/hpc/projects/virtual_staining/viral_sensor_test_dataio/2024_02_04_A549_DENV_ZIKV_timelapse/6-patches/full_patch.zarr"
+channels = 1
+x = 200
+y = 200
+z = 12 # 15 for covnext backbone, 12 for resnet
+z_range = (26, 38) # (28, 43) for covnext backbone, (26, 38) for resnet
+batch_size = 32
+channel_names = ["RFP"]  # training w/ both channels
+
+torch.set_float32_matmul_precision("medium")
+
+# contra_model = ContrastiveEncoder(backbone="resnet50")
+# print(contra_model)
+
+# model_graph = torchview.draw_graph(
+#     contra_model,
+#     torch.randn(1, 1, 15, 200, 200),
+#     depth=3,
+#     device="cpu",
+# )
+# model_graph.visual_graph
+
+# contrastive_module = ContrastiveModule()
+# print(contrastive_module.encoder)
+
+# model_graph = torchview.draw_graph(
+#     contrastive_module.encoder,
+#     torch.randn(1, 1, 15, 200, 200),
+#     depth=3,
+#     device="cpu",
+# )
+# model_graph.visual_graph
+
+
+# %% Define the main function for training
+def main(hparams):
+    # Seed for reproducibility
+    # seed_everything(42, workers=True)
+
+    num_gpus = torch.cuda.device_count()
+    print(f"Number of GPUs available: {num_gpus}")
+
+    print("Starting data module..")
+    # Initialize the data module
+    data_module = ContrastiveDataModule(
+        base_path=base_path,
+        channels=channels,
+        x=x,
+        y=y,
+        timesteps_csv_path=timesteps_csv_path,
+        channel_names=channel_names,
+        batch_size=batch_size,
+        z_range=z_range,
+    )
+
+    print("data module set up!")
+
+    # Setup the data module for training, val and testing
+    data_module.setup(stage="fit")
+
+    print(
+        f"Total dataset size: {len(data_module.train_dataset) + len(data_module.val_dataset) + len(data_module.test_dataset)}"
+    )
+    print(f"Training dataset size: {len(data_module.train_dataset)}")
+    print(f"Validation dataset size: {len(data_module.val_dataset)}")
+    print(f"Test dataset size: {len(data_module.test_dataset)}")
+
+    # Initialize the model
+    model = ContrastiveModule(
+        backbone=hparams.backbone,
+        loss_function=torch.nn.TripletMarginLoss(),
+        margin=hparams.margin,
+        lr=hparams.lr,
+        schedule=hparams.schedule,
+        log_steps_per_epoch=hparams.log_steps_per_epoch,
+        in_channels=channels,
+        example_input_yx_shape=(x, y),
+        in_stack_depth=z, # only thing to control 
+        stem_kernel_size=(5, 3, 3), # keep fixed for both 
+        embedding_len=hparams.embedding_len,
+    )
+
+    # Initialize logger
+    wandb_logger = WandbLogger(project="contrastive_model", log_model="all")
+
+    # set for each run to avoid overwritting!
+    custom_folder_name = "rfp_resnet_stem"
+    checkpoint_callback = ModelCheckpoint(
+        dirpath=os.path.join(model_dir, custom_folder_name),
+        filename="contrastive_model-test-{epoch:02d}-{val_loss:.2f}",
+        save_top_k=3,
+        mode="min",
+        monitor="val/loss_epoch",
+    )
+
+    trainer = Trainer(
+        max_epochs=hparams.max_epochs,
+        callbacks=[checkpoint_callback],
+        logger=wandb_logger,
+        accelerator=hparams.accelerator,
+        devices=hparams.devices,
+        num_nodes=hparams.num_nodes,
+        strategy=DDPStrategy(),
+        log_every_n_steps=hparams.log_every_n_steps,
+        num_sanity_val_steps=0,
+    )
+
+    train_loader = data_module.train_dataloader()
+    example_batch = next(iter(train_loader))
+    example_input = example_batch[0]
+
+    wandb_logger.watch(model, log="all", log_graph=(example_input,))
+
+    # Fetches batches from the training dataloader,
+    # Calls the training_step method on the model for each batch
+    # Aggregates the losses and performs optimization steps
+    trainer.fit(model, datamodule=data_module)
+
+    # Validate the model
+    trainer.validate(model, datamodule=data_module)
+
+    # Test the model
+    trainer.test(model, datamodule=data_module)
+
+# Argument parser for command-line options
+# to-do: need to clean up to always use the same args
+parser = ArgumentParser()
+parser.add_argument("--backbone", type=str, default="resnet50")
+parser.add_argument("--margin", type=float, default=0.5)
+parser.add_argument("--lr", type=float, default=1e-3)
+parser.add_argument("--schedule", type=str, default="Constant")
+parser.add_argument("--log_steps_per_epoch", type=int, default=10)
+parser.add_argument("--embedding_len", type=int, default=256)
+parser.add_argument("--max_epochs", type=int, default=200)
+parser.add_argument("--accelerator", type=str, default="gpu")
+parser.add_argument("--devices", type=int, default=1)  # 4 GPUs
+parser.add_argument("--num_nodes", type=int, default=1)
+parser.add_argument("--log_every_n_steps", type=int, default=1)
+args = parser.parse_args()
+
+main(args)
+
