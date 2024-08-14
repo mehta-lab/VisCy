27f00762d6ce5588d82903d1315a3d61113f13eb
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-08-14 09:22:37 -0700

    simplify env var in job script and match cpu count with config

--- applications/contrastive_phenotyping/contrastive_cli/predict_slurm.sh
+++ applications/contrastive_phenotyping/contrastive_cli/predict_slurm.sh
@@ -5,12 +5,13 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --partition=gpu
-#SBATCH --cpus-per-task=64
+#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=7G
 #SBATCH --time=0-01:00:00
 
 module load anaconda/2022.05
-conda activate /hpc/mydata/$USER/envs/viscy
+# Update to use the actual prefix
+conda activate $MYDATA/envs/viscy
 
 scontrol show job $SLURM_JOB_ID
 
