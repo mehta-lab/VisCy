c3edf1bcb51d39be9bbd2fbd76eb93d279ae8aee
Soorya Pradeep <soorya.pradeep@czbiohub.org>
2024-08-27 11:37:50 -0700

    for entire well

--- applications/contrastive_phenotyping/contrastive_cli/PC_vs_CF.py
+++ applications/contrastive_phenotyping/contrastive_cli/PC_vs_CF.py
@@ -29,163 +29,180 @@ tracks_path = Path(
 source_channel = ["Phase3D", "RFP"]
 z_range = (28, 43)
 normalizations = None
-fov_name = "/B/4/5"
-track_id = 11
+# fov_name = "/B/4/5"
+# track_id = 11
 
 embedding_dataset = read_embedding_dataset(features_path)
 embedding_dataset
-all_tracks_FOV = embedding_dataset.sel(fov_name=fov_name)
-a_track_in_FOV = all_tracks_FOV.sel(track_id=track_id)
-# Why is sample dimension ~22000 long after the dataset is sliced by FOV and by track_id?
-indices = np.arange(a_track_in_FOV.sizes["sample"])
-features_track = a_track_in_FOV["features"]
-time_stamp = features_track["t"][indices].astype(str)
 
-scaled_features_track = StandardScaler().fit_transform(features_track.values)
+unique_fov_names = [name for name in embedding_dataset["fov_name"].values if name.startswith("/A/3/")]
+correlation_sum = pd.DataFrame()
+ii = 0
 
-# %% perform PCA analysis of features
+for fov_name in unique_fov_names:
 
-pca = PCA(n_components=5)
-pca_features = pca.fit_transform(scaled_features_track)
+    all_tracks_FOV = embedding_dataset.sel(fov_name=fov_name)
 
-features_track = (
-    features_track.assign_coords(PCA1=("sample", pca_features[:, 0]))
-    .assign_coords(PCA2=("sample", pca_features[:, 1]))
-    .assign_coords(PCA3=("sample", pca_features[:, 2]))
-    .assign_coords(PCA4=("sample", pca_features[:, 3]))
-    .assign_coords(PCA5=("sample", pca_features[:, 4]))
-    .set_index(sample=["PCA1", "PCA2", "PCA3", "PCA4", "PCA5"], append=True)
-)
+    unique_track_ids = list(all_tracks_FOV["track_id"].values)
+    unique_track_ids = list(set(unique_track_ids))
 
-# %% load the image patches
+    for track_id in unique_track_ids:
+        a_track_in_FOV = all_tracks_FOV.sel(track_id=track_id)
+        indices = np.arange(a_track_in_FOV.sizes["sample"])
+        features_track = a_track_in_FOV["features"]
+        time_stamp = features_track["t"][indices].astype(str)
 
-data_module = TripletDataModule(
-    data_path=data_path,
-    tracks_path=tracks_path,
-    source_channel=source_channel,
-    z_range=z_range,
-    initial_yx_patch_size=(256, 256),
-    final_yx_patch_size=(256, 256),
-    batch_size=1,
-    num_workers=16,
-    normalizations=normalizations,
-    predict_cells=True,
-    include_fov_names=[fov_name],
-    include_track_ids=[track_id],
-)
-# for train and val
-data_module.setup("predict")
-predict_dataset = data_module.predict_dataset
+        scaled_features_track = StandardScaler().fit_transform(features_track.values)
 
-phase = np.stack([p["anchor"][0, 7].numpy() for p in predict_dataset])
-fluor = np.stack([np.max(p["anchor"][1].numpy(), axis=0) for p in predict_dataset])
+        # perform PCA analysis of features
 
-# %% first feature: symmetry of cell
+        pca = PCA(n_components=5)
+        if scaled_features_track.shape[0] > 5:
+            pca_features = pca.fit_transform(scaled_features_track)
+            ii += 1
+        else:
+            continue
 
-# Compute Fourier descriptors for phase image
-data = {
-    "Phase Symmetry Score": [],
-    "Fluor Symmetry Score": [],
-    "Area": [],
-    "Entropy Phase": [],
-    "Entropy Fluor": [],
-    "Contrast Phase": [],
-    "Dissimilarity Phase": [],
-    "Homogeneity Phase": [],
-    "Contrast Fluor": [],
-    "Dissimilarity Fluor": [],
-    "Homogeneity Fluor": [],
-    # "Edge Density Phase": [],
-    # "Edge Density Fluor": [],
-    "IQR": [],
-    "Mean Intensity": [],
-    "Standard Deviation": [],
-}
+        features_track = (
+            features_track.assign_coords(PCA1=("sample", pca_features[:, 0]))
+            .assign_coords(PCA2=("sample", pca_features[:, 1]))
+            .assign_coords(PCA3=("sample", pca_features[:, 2]))
+            .assign_coords(PCA4=("sample", pca_features[:, 3]))
+            .assign_coords(PCA5=("sample", pca_features[:, 4]))
+            .set_index(sample=["PCA1", "PCA2", "PCA3", "PCA4", "PCA5"], append=True)
+        )
 
-for t in range(phase.shape[0]):
-    # Compute Fourier descriptors for phase image
-    phase_descriptors = FE.compute_fourier_descriptors(phase[t])
-    # Analyze symmetry of phase image
-    phase_symmetry_score = FE.analyze_symmetry(phase_descriptors)
+        # load the image patches
 
-    # Compute Fourier descriptors for fluor image
-    fluor_descriptors = FE.compute_fourier_descriptors(fluor[t])
-    # Analyze symmetry of fluor image
-    fluor_symmetry_score = FE.analyze_symmetry(fluor_descriptors)
+        data_module = TripletDataModule(
+            data_path=data_path,
+            tracks_path=tracks_path,
+            source_channel=source_channel,
+            z_range=z_range,
+            initial_yx_patch_size=(256, 256),
+            final_yx_patch_size=(256, 256),
+            batch_size=1,
+            num_workers=16,
+            normalizations=normalizations,
+            predict_cells=True,
+            include_fov_names=[fov_name],
+            include_track_ids=[track_id],
+        )
+        # for train and val
+        data_module.setup("predict")
+        predict_dataset = data_module.predict_dataset
 
-    # Compute area of sensor
-    thresh_img, area = FE.compute_area(fluor[t])
+        phase = np.stack([p["anchor"][0, 7].numpy() for p in predict_dataset])
+        fluor = np.stack([np.max(p["anchor"][1].numpy(), axis=0) for p in predict_dataset])
 
-    # Compute higher frequency features using spectral entropy
-    entropy_phase = FE.compute_spectral_entropy(phase[t])
-    entropy_fluor = FE.compute_spectral_entropy(fluor[t])
+        # Compute Fourier descriptors for phase image
+        data = {
+            "Phase Symmetry Score": [],
+            "Fluor Symmetry Score": [],
+            "Sensor Area": [],
+            "Entropy Phase": [],
+            "Entropy Fluor": [],
+            "Contrast Phase": [],
+            "Dissimilarity Phase": [],
+            "Homogeneity Phase": [],
+            "Contrast Fluor": [],
+            "Dissimilarity Fluor": [],
+            "Homogeneity Fluor": [],
+            # "Edge Density Phase": [],
+            # "Edge Density Fluor": [],
+            "Phase IQR": [],
+            "Fluor Mean Intensity": [],
+            "Standard Deviation": [],
+        }
 
-    # Compute texture analysis using GLCM
-    contrast_phase, dissimilarity_phase, homogeneity_phase = FE.compute_glcm_features(phase[t])
-    contrast_fluor, dissimilarity_fluor, homogeneity_fluor = FE.compute_glcm_features(fluor[t])
+        for t in range(phase.shape[0]):
+            # Compute Fourier descriptors for phase image
+            phase_descriptors = FE.compute_fourier_descriptors(phase[t])
+            # Analyze symmetry of phase image
+            phase_symmetry_score = FE.analyze_symmetry(phase_descriptors)
 
-    # # Compute edge detection using Canny
-    # edges_phase = FE.detect_edges(phase[t])
-    # edges_fluor = FE.detect_edges(fluor[t])
+            # Compute Fourier descriptors for fluor image
+            fluor_descriptors = FE.compute_fourier_descriptors(fluor[t])
+            # Analyze symmetry of fluor image
+            fluor_symmetry_score = FE.analyze_symmetry(fluor_descriptors)
 
-    # Quantify the amount of edge feature in the phase image
-    # edge_density_phase = np.sum(edges_phase) / (edges_phase.shape[0] * edges_phase.shape[1])
+            # Compute area of sensor
+            thresh_img, area = FE.compute_area(fluor[t])
 
-    # Quantify the amount of edge feature in the fluor image
-    # edge_density_fluor = np.sum(edges_fluor) / (edges_fluor.shape[0] * edges_fluor.shape[1])
+            # Compute higher frequency features using spectral entropy
+            entropy_phase = FE.compute_spectral_entropy(phase[t])
+            entropy_fluor = FE.compute_spectral_entropy(fluor[t])
 
-    # Compute interqualtile range of pixel intensities
-    iqr = FE.compute_iqr(phase[t])
+            # Compute texture analysis using GLCM
+            contrast_phase, dissimilarity_phase, homogeneity_phase = FE.compute_glcm_features(phase[t])
+            contrast_fluor, dissimilarity_fluor, homogeneity_fluor = FE.compute_glcm_features(fluor[t])
 
-    # Compute mean pixel intensity
-    mean_intensity = FE.compute_mean_intensity(fluor[t])
+            # # Compute edge detection using Canny
+            # edges_phase = FE.detect_edges(phase[t])
+            # edges_fluor = FE.detect_edges(fluor[t])
 
-    # Compute standard deviation of pixel intensities
-    std_dev = FE.compute_std_dev(phase[t])
+            # Quantify the amount of edge feature in the phase image
+            # edge_density_phase = np.sum(edges_phase) / (edges_phase.shape[0] * edges_phase.shape[1])
 
-    # Append the computed features to the data dictionary
-    data["Phase Symmetry Score"].append(phase_symmetry_score)
-    data["Fluor Symmetry Score"].append(fluor_symmetry_score)
-    data["Area"].append(area)
-    data["Entropy Phase"].append(entropy_phase)
-    data["Entropy Fluor"].append(entropy_fluor)
-    data["Contrast Phase"].append(contrast_phase)
-    data["Dissimilarity Phase"].append(dissimilarity_phase)
-    data["Homogeneity Phase"].append(homogeneity_phase)
-    data["Contrast Fluor"].append(contrast_fluor)
-    data["Dissimilarity Fluor"].append(dissimilarity_fluor)
-    data["Homogeneity Fluor"].append(homogeneity_fluor)
-    # data["Edge Density Phase"].append(edge_density_phase)
-    # data["Edge Density Fluor"].append(edge_density_fluor)
-    data["IQR"].append(iqr)
-    data["Mean Intensity"].append(mean_intensity)
-    data["Standard Deviation"].append(std_dev)
+            # Quantify the amount of edge feature in the fluor image
+            # edge_density_fluor = np.sum(edges_fluor) / (edges_fluor.shape[0] * edges_fluor.shape[1])
 
-# Create a dataframe to store the computed features
-features = pd.DataFrame(data)
+            # Compute interqualtile range of pixel intensities
+            iqr = FE.compute_iqr(phase[t])
 
-# %% compute correlation between PCA features and computed features
+            # Compute mean pixel intensity
+            fluor_mean_intensity = FE.compute_mean_intensity(fluor[t])
 
-# Create a dataframe with PCA results
-pca_results = pd.DataFrame(pca_features, columns=["PCA1", "PCA2", "PCA3", "PCA4", "PCA5"])
+            # Compute standard deviation of pixel intensities
+            std_dev = FE.compute_std_dev(phase[t])
 
-# Compute correlation between PCA features and computed features
-correlation = pd.concat([pca_results, features], axis=1).corr()
-correlation
+            # Append the computed features to the data dictionary
+            data["Phase Symmetry Score"].append(phase_symmetry_score)
+            data["Fluor Symmetry Score"].append(fluor_symmetry_score)
+            data["Sensor Area"].append(area)
+            data["Entropy Phase"].append(entropy_phase)
+            data["Entropy Fluor"].append(entropy_fluor)
+            data["Contrast Phase"].append(contrast_phase)
+            data["Dissimilarity Phase"].append(dissimilarity_phase)
+            data["Homogeneity Phase"].append(homogeneity_phase)
+            data["Contrast Fluor"].append(contrast_fluor)
+            data["Dissimilarity Fluor"].append(dissimilarity_fluor)
+            data["Homogeneity Fluor"].append(homogeneity_fluor)
+            # data["Edge Density Phase"].append(edge_density_phase)
+            # data["Edge Density Fluor"].append(edge_density_fluor)
+            data["Phase IQR"].append(iqr)
+            data["Fluor Mean Intensity"].append(fluor_mean_intensity)
+            data["Standard Deviation"].append(std_dev)
+
+        # Create a dataframe to store the computed features
+        features = pd.DataFrame(data)
+
+        # compute correlation between PCA features and computed features
+
+        # Create a dataframe with PCA results
+        pca_results = pd.DataFrame(pca_features, columns=["PCA1", "PCA2", "PCA3", "PCA4", "PCA5"])
+
+        # Compute correlation between PCA features and computed features
+        correlation = pd.concat([pca_results, features], axis=1).corr()
+        correlation_sum = correlation_sum.add(correlation, fill_value=0)
+
+correlation_avg = correlation_sum / ii
 
 # %% find the best correlated computed features with PCA features
 
 # Find the best correlated computed features with PCA features
-best_correlated_features = correlation.loc["PCA1":"PCA5", :].idxmax()
+best_correlated_features = correlation_avg.loc["PCA1":"PCA5", :].idxmax()
 best_correlated_features
 
 # display as a heatmap
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-plt.figure(figsize=(10, 10))
-sns.heatmap(correlation, annot=True, cmap="coolwarm", fmt=".2f")
+plt.figure(figsize=(20, 5))
+sns.heatmap(correlation_avg.drop(columns=["PCA1", "PCA2", "PCA3", "PCA4", "PCA5"]).loc["PCA1":"PCA5", :], annot=True, cmap="coolwarm", fmt=".2f")
 plt.title("Correlation between PCA features and computed features")
+plt.xlabel("Computed Features")
+plt.ylabel("PCA Features")
 plt.show()
 
 # %%
