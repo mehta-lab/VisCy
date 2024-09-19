94086268c0d7eb9c61d310df0160854b402b54b8
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-09-19 10:19:51 -0700

    compute metrics for all models

--- applications/contrastive_phenotyping/evaluation/linear_probing.py
+++ applications/contrastive_phenotyping/evaluation/linear_probing.py
@@ -8,36 +8,47 @@ from viscy.representation.lca import fit_logistic_regression
 # %%
 TRAIN_FOVS = ["/A/3/7", "/A/3/8", "/A/3/9", "/B/4/6", "/B/4/7"]
 
-path_embedding = Path(
-    "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/models/time_sampling_strategies/time_interval/predict/feb_test_time_interval_1_epoch_178.zarr"
-)
+
+model_embeddings = {
+    "no-track": Path(
+        "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/models/time_sampling_strategies/negpair_random_sampling2/feb_fixed_test_predict.zarr"
+    ),
+    "cell-aware-2ch": Path(
+        "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/models/time_sampling_strategies/negpair_difcell_randomtime_sampling/Ver2_updateTracking_refineModel/predictions/Feb_2chan_128patch_32projDim/2chan_128patch_56ckpt_FebTest.zarr"
+    ),
+    "cell-aware-1ch": Path(
+        "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/models/time_sampling_strategies/negpair_difcell_randomtime_sampling/Ver2_updateTracking_refineModel/predictions/Feb_1chan_128patch_32projDim/1chan_128patch_63ckpt_FebTest.zarr"
+    ),
+    "time-cell-aware": Path(
+        "/hpc/projects/intracellular_dashboard/viral-sensor/infection_classification/models/time_sampling_strategies/time_interval/predict/feb_test_time_interval_1_epoch_178.zarr"
+    ),
+}
 path_annotations_infection = Path(
     "/hpc/projects/intracellular_dashboard/viral-sensor/2024_02_04_A549_DENV_ZIKV_timelapse/8-train-test-split/supervised_inf_pred/extracted_inf_state.csv"
 )
 
 # %%
-dataset = read_embedding_dataset(path_embedding)
-features = dataset["features"]
-features
+for model_name, path_embedding in model_embeddings.items():
+    print(f"Model: {model_name}")
+    dataset = read_embedding_dataset(path_embedding)
+    features = dataset["features"]
 
-# %%
-infection = load_annotation(
-    dataset,
-    path_annotations_infection,
-    "infection_state",
-    {0.0: "background", 1.0: "uninfected", 2.0: "infected"},
-)
-infection
+    infection = load_annotation(
+        dataset,
+        path_annotations_infection,
+        "infection_state",
+        {0.0: "background", 1.0: "uninfected", 2.0: "infected"},
+    )
 
-# %%
-log_reg = fit_logistic_regression(
-    features,
-    infection,
-    train_fovs=TRAIN_FOVS,
-    remove_background_class=True,
-    scale_features=False,
-    class_weight="balanced",
-    solver="liblinear",
-)
+    log_reg = fit_logistic_regression(
+        features,
+        infection,
+        train_fovs=TRAIN_FOVS,
+        remove_background_class=True,
+        scale_features=False,
+        class_weight="balanced",
+        solver="liblinear",
+        random_state=42,
+    )
 
 # %%
