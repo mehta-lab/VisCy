fe94a305aada5a20978629d12f516be9e11484c6
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-09-20 14:00:42 -0700

    tweak contrast limits and text

--- applications/contrastive_phenotyping/evaluation/grad_attr.py
+++ applications/contrastive_phenotyping/evaluation/grad_attr.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import torch
-from captum.attr import IntegratedGradients
 from cmap import Colormap
 from lightning.pytorch import seed_everything
 from skimage.exposure import rescale_intensity
@@ -17,13 +16,11 @@ from viscy.representation.engine import ContrastiveEncoder, ContrastiveModule
 from viscy.representation.evaluation import load_annotation
 from viscy.representation.lca import (
     AssembledClassifier,
+    attribute_sample_binary,
     fit_logistic_regression,
     linear_from_binary_logistic_regression,
 )
 from viscy.transforms import NormalizeSampled, ScaleIntensityRangePercentilesd
-
-# %%
-plt.style.use("./figure.mplstyle")
 
 # %%
 seed_everything(42, workers=True)
@@ -166,19 +163,6 @@ track_classes_division = track_classes_division[
 
 
 # %%
-def attribute_sample(img, assembled_classifier):
-    ig = IntegratedGradients(assembled_classifier, multiply_by_inputs=True)
-    assembled_classifier.zero_grad()
-    attribution = ig.attribute(torch.from_numpy(img)).numpy()
-    return img, attribution
-
-
-def clim(heatmap, low=1, high=99):
-    lo, hi = np.percentile(heatmap, (low, high))
-    return rescale_intensity(heatmap.clip(lo, hi), out_range=(0, 1))
-
-
-# %%
 for sample in dm.predict_dataloader():
     img = sample["anchor"].numpy()
 
@@ -186,33 +170,64 @@ for sample in dm.predict_dataloader():
 with torch.inference_mode():
     infection_probs = assembled_classifier_infection(torch.from_numpy(img)).sigmoid()
     division_probs = assembled_classifier_division(torch.from_numpy(img)).sigmoid()
-img, infection_attribution = attribute_sample(img, assembled_classifier_infection)
-_, division_attribution = attribute_sample(img, assembled_classifier_division)
 
 # %%
+img, infection_attribution = attribute_sample_binary(
+    img,
+    assembled_classifier_infection,
+    # multiply_by_inputs=False,
+)
+_, division_attribution = attribute_sample_binary(
+    img,
+    assembled_classifier_division,
+    # multiply_by_inputs=False,
+)
+
+
+# %%
+g_lim = 2e-3
+
+
+def clip_rescale(img, low, high):
+    return rescale_intensity(img.clip(low, high), out_range=(0, 1))
+
+
+def clim_percentile(heatmap, low=1, high=99):
+    lo, hi = np.percentile(heatmap, (low, high))
+    return clip_rescale(heatmap, lo, hi)
+
+
 z_slice = 5
-phase = clim(img[:, 0, z_slice])
-rfp = clim(img[:, 1, z_slice])
+phase = clim_percentile(img[:, 0, z_slice])
+rfp = clim_percentile(img[:, 1, z_slice])
 img_render = np.concatenate([phase, rfp], axis=2)
-phase_heatmap_inf = clim(infection_attribution[:, 0, z_slice])
-rfp_heatmap_inf = clim(infection_attribution[:, 1, z_slice])
-inf_render = np.concatenate([phase_heatmap_inf, rfp_heatmap_inf], axis=2)
-phase_heatmap_div = clim(division_attribution[:, 0, z_slice])
-rfp_heatmap_div = clim(division_attribution[:, 1, z_slice])
-div_render = np.concatenate([phase_heatmap_div, rfp_heatmap_div], axis=2)
+phase_heatmap_inf = infection_attribution[:, 0, z_slice]
+rfp_heatmap_inf = infection_attribution[:, 1, z_slice]
+inf_render = clip_rescale(
+    np.concatenate([phase_heatmap_inf, rfp_heatmap_inf], axis=2), -g_lim, g_lim
+)
+phase_heatmap_div = division_attribution[:, 0, z_slice]
+rfp_heatmap_div = division_attribution[:, 1, z_slice]
+div_render = clip_rescale(
+    np.concatenate([phase_heatmap_div, rfp_heatmap_div], axis=2), -g_lim, g_lim
+)
+
 
 # %%
+plt.style.use("./figure.mplstyle")
+
 selected_time_points = [3, 6, 15, 16]
 selected_div_states = [False] * 3 + [True]
 
 sps = len(selected_time_points)
 
 icefire = Colormap("icefire").to_mpl()
+
 f, ax = plt.subplots(3, sps, figsize=(5.5, 3), layout="compressed")
 for i, time in enumerate(selected_time_points):
     hpi = 3 + 0.5 * time
     prob = infection_probs[time].item()
-    inf_binary = str(bool(track_classes_infection.iloc[time])).lower()
+    inf_binary = str(bool(track_classes_infection.iloc[time] - 1)).lower()
     div_binary = str(selected_div_states[i]).lower()
     ax[0, i].imshow(img_render[time], cmap="gray")
     ax[0, i].set_title(f"{hpi} HPI")
@@ -226,14 +241,14 @@ for i, time in enumerate(selected_time_points):
     )
 for a in ax.ravel():
     a.axis("off")
-norm = mpl.colors.Normalize(vmin=-1, vmax=1)
+norm = mpl.colors.Normalize(vmin=-g_lim, vmax=g_lim)
 cbar = f.colorbar(
     mpl.cm.ScalarMappable(norm=norm, cmap=icefire),
     orientation="vertical",
     ax=ax[1:].ravel().tolist(),
+    format=mpl.ticker.StrMethodFormatter("{x}")
 )
 cbar.set_label("integrated gradients")
-plt.show()
 
 # %%
 f.savefig(
