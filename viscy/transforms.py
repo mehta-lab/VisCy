2e37217285c5329be1f934c9e6413cd7dc1abfa4
Ziwen Liu <ziwen.liu@czbiohub.org>
2024-10-30 16:44:54 -0700

    add tiled crop transform for validation

--- viscy/transforms.py
+++ viscy/transforms.py
@@ -2,8 +2,11 @@
 
 from typing import Sequence, Union
 
+import numpy as np
+import torch
 from monai.transforms import (
     MapTransform,
+    MultiSampleTrait,
     RandAdjustContrastd,
     RandAffined,
     RandGaussianNoised,
@@ -15,9 +18,10 @@ from monai.transforms import (
 )
 from monai.transforms.transform import Randomizable
 from numpy.random.mtrand import RandomState as RandomState
+from torch import Tensor
 from typing_extensions import Iterable, Literal
 
-from viscy.data.typing import Sample
+from viscy.data.typing import ChannelMap, Sample
 
 
 class RandWeightedCropd(RandWeightedCropd):
@@ -231,3 +235,53 @@ class RandInvertIntensityd(MapTransform, RandomizableTransform):
     ) -> Randomizable:
         super().set_random_state(seed, state)
         return self
+
+
+class TiledSpatialCropSamplesd(MapTransform, MultiSampleTrait):
+    """
+    Crop multiple tiled ROIs from an image.
+    Used for deterministic cropping in validation.
+    """
+
+    def __init__(
+        self,
+        keys: Union[str, Iterable[str]],
+        roi_size: tuple[int, int, int],
+        num_samples: int,
+    ) -> None:
+        super().__init__(keys, allow_missing_keys=False)
+        self.roi_size = roi_size
+        self.num_samples = num_samples
+
+    def _check_num_samples(self, spatial_size: np.ndarray, offset: int) -> np.ndarray:
+        max_grid_shape = spatial_size // self.roi_size
+        max_num_samples = max_grid_shape.prod()
+        if offset >= max_num_samples:
+            raise ValueError(
+                f"Number of samples {self.num_samples} should be "
+                f"smaller than {max_num_samples}."
+            )
+        grid_idx = np.asarray(np.unravel_index(offset, max_grid_shape))
+        return grid_idx * self.roi_size
+
+    def _crop(self, img: Tensor, offset: int) -> Tensor:
+        spatial_size = np.array(img.shape[-3:])
+        crop_start = self._check_num_samples(spatial_size, offset)
+        crop_end = crop_start + np.array(self.roi_size)
+        return img[
+            ...,
+            crop_start[0] : crop_end[0],
+            crop_start[1] : crop_end[1],
+            crop_start[2] : crop_end[2],
+        ]
+
+    def __call__(self, sample: Sample) -> Sample:
+        results = []
+        for i in range(self.num_samples):
+            result = {}
+            for key in self.keys:
+                result[key] = self._crop(sample[key], i)
+            if "norm_meta" in sample:
+                result["norm_meta"] = sample["norm_meta"]
+            results.append(result)
+        return results
