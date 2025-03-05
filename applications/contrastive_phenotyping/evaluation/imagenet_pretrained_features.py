3524155f7547b994d010c94e44154b663b569b2a
Soorya Pradeep <soorya.pradeep@czbiohub.org>
2025-03-05 09:36:06 -0800

    add human annotation for hue

--- applications/contrastive_phenotyping/evaluation/imagenet_pretrained_features.py
+++ applications/contrastive_phenotyping/evaluation/imagenet_pretrained_features.py
@@ -8,6 +8,7 @@ import torch
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 from tqdm import tqdm
+from pathlib import Path
 
 from viscy.data.triplet import TripletDataModule
 from viscy.transforms import ScaleIntensityRangePercentilesd
@@ -17,8 +18,8 @@ model = timm.create_model("convnext_tiny", pretrained=True).eval().to("cuda")
 
 # %%
 dm = TripletDataModule(
-    data_path="/hpc/reference/imaging/ALFI_dataset/Analysis/float_phase_ome_zarr_output_test.zarr",
-    tracks_path="/hpc/reference/imaging/ALFI_dataset/Analysis/track_phase_ome_zarr_output_test.zarr",
+    data_path="/hpc/projects/organelle_phenotyping/ALFI_models_data/datasets/zarr_datasets/float_phase_ome_zarr_output_test.zarr",
+    tracks_path="/hpc/projects/organelle_phenotyping/ALFI_models_data/datasets/zarr_datasets/track_phase_ome_zarr_output_test.zarr",
     source_channel=["DIC"],
     z_range=(0, 1),
     batch_size=128,
@@ -54,11 +55,35 @@ scaled_features = StandardScaler().fit_transform(pooled)
 pca = PCA(n_components=2)
 pca_features = pca.fit_transform(scaled_features)
 
+# %% add pooled to dataframe naming each column with feature_i
+for i, feature in enumerate(pooled.T):
+    tracks[f"feature_{i}"] = feature
+# add pca features to dataframe naming each column with pca_i
+for i, feature in enumerate(pca_features.T):
+    tracks[f"pca_{i}"] = feature
+
+# # save the dataframe as csv
+# tracks.to_csv("/hpc/projects/comp.micro/infected_cell_imaging/Single_cell_phenotyping/ContrastiveLearning/code/ALFI/imagenet_pretrained_features.csv", index=False)
+
+# %% load the dataframe
+# tracks = pd.read_csv("/hpc/projects/comp.micro/infected_cell_imaging/Single_cell_phenotyping/ContrastiveLearning/code/ALFI/imagenet_pretrained_features.csv")
+
+# %% load annotations
+
+ann_root = Path(
+    "/hpc/projects/organelle_phenotyping/ALFI_models_data/datasets/zarr_datasets"
+)
+ann_path = ann_root / "test_annotations.csv"
+annotation = pd.read_csv(ann_path)
+
+# add division column from annotation to tracks
+tracks["division"] = annotation["division"]
+
 # %%
-ax = sns.kdeplot(
-    x=pca_features[:, 0],
-    y=pca_features[:, 1],
-    hue=tracks["fov_name"].rename("FOV name"),
+ax = sns.scatterplot(
+    x=tracks["pca_0"],
+    y=tracks["pca_1"],
+    hue=tracks["division"],
     legend="full",
 )
 ax.set_xlabel("PCA1")
