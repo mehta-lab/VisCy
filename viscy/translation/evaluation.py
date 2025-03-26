391db505a5c2d4918dff1aa1a8fc02f0137560ce
Ziwen Liu <ziwen.liu@czbiohub.org>
2025-03-26 09:47:15 -0700

    fix dice score parameters

--- viscy/translation/evaluation.py
+++ viscy/translation/evaluation.py
@@ -37,7 +37,11 @@ class SegmentationMetrics2D(LightningModule):
                 "position": batch["position_idx"][0],
                 "time": batch["time_idx"][0],
                 "accuracy": (accuracy(pred_binary, target_binary, task="binary")),
-                "dice": (dice_score(pred_binary, target_binary)),
+                "dice": (
+                    dice_score(
+                        pred_binary, target_binary, num_classes=2, input_format="index"
+                    )
+                ),
                 "jaccard": (jaccard_index(pred_binary, target_binary, task="binary")),
                 "mAP": coco_metrics["map"],
                 "mAP_50": coco_metrics["map_50"],
