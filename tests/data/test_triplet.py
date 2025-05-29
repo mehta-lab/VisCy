32262439c7216d7ba1684ba85f2e2762b74cfdcb
Ziwen Liu <ziwen.liu@czbiohub.org>
2025-05-29 13:55:49 -0700

    test exclude fovs

--- tests/data/test_triplet.py
+++ tests/data/test_triplet.py
@@ -5,8 +5,9 @@ from viscy.data.triplet import TripletDataModule
 
 
 @mark.parametrize("include_wells", [None, ["A/1", "B/2"]])
+@mark.parametrize("exclude_fovs", [None, ["0", "1"]])
 def test_datamodule_setup_fit(
-    preprocessed_hcs_dataset, tracks_hcs_dataset, include_wells
+    preprocessed_hcs_dataset, tracks_hcs_dataset, include_wells, exclude_fovs
 ):
     data_path = preprocessed_hcs_dataset
     z_window_size = 5
@@ -15,6 +16,16 @@ def test_datamodule_setup_fit(
     batch_size = 4
     with open_ome_zarr(data_path) as dataset:
         channel_names = dataset.channel_names
+        total_wells = len(list(dataset.wells()))
+        fovs_per_well = len(dataset["A/2"])
+    if include_wells is not None:
+        total_wells -= len(include_wells)
+    if exclude_fovs is not None:
+        fovs_per_well -= len(exclude_fovs)
+    total_fovs = total_wells * fovs_per_well
+    len_total = total_fovs * 2
+    len_train = int(len_total * split_ratio)
+    len_val = len_total - len_train
     dm = TripletDataModule(
         data_path=data_path,
         tracks_path=tracks_hcs_dataset,
@@ -26,15 +37,12 @@ def test_datamodule_setup_fit(
         split_ratio=split_ratio,
         batch_size=batch_size,
         fit_include_wells=include_wells,
+        fit_exclude_fovs=exclude_fovs,
         return_negative=True,
     )
     dm.setup(stage="fit")
-    if include_wells is not None:
-        assert len(dm.train_dataset) == 6
-        assert len(dm.val_dataset) == 2
-    else:
-        assert len(dm.train_dataset) == 12
-        assert len(dm.val_dataset) == 4
+    assert len(dm.train_dataset) == len_train
+    assert len(dm.val_dataset) == len_val
     for batch in dm.train_dataloader():
         assert batch["anchor"].shape == (
             batch_size,
