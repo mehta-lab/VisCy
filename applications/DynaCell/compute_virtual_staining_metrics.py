8da84e9fe6feb60f008d18d54089c93c48004a3f
Ivan Ivanov <ivan.ivanov@czbiohub.org>
2025-09-08 14:53:07 -0700

    vs metrics v1

--- applications/DynaCell/compute_virtual_staining_metrics.py
+++ applications/DynaCell/compute_virtual_staining_metrics.py
@@ -17,7 +17,7 @@ from viscy.translation.evaluation import IntensityMetrics, SegmentationMetrics
 csv_database_path = Path(
     "~/gdrive/publications/dynacell/summary_table/dynacell_summary_table_2025_05_05.csv"
 ).expanduser()
-output_dir = Path("~/gdrive/publications/dynacell/metrics/virtual_staining").expanduser()
+output_dir = Path("~/Documents/dynacell//metrics/virtual_staining").expanduser()
 output_dir.mkdir(parents=True, exist_ok=True)
 
 database = pd.read_csv(csv_database_path, dtype={"FOV": str})
@@ -38,7 +38,7 @@ metrics = compute_metrics(
     prediction_database=database,
     prediction_channel_name="Nuclei-prediction",
     log_output_dir=output_dir,
-    log_name="intensity_VSCyto3D",
+    log_name="intensity_VSCyto3D_HEK293T_nuclei_mock",
     z_slice=36,
     transforms=[
         NormalizeIntensityd(
@@ -47,7 +47,102 @@ metrics = compute_metrics(
     ],
 )
 
-# %% Compute VSCyto3D segmentation-based metrics
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["HEK293T"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["Mock"],
+    target_database=database,
+    target_channel_name="Membrane",
+    prediction_database=database,
+    prediction_channel_name="Membrane-prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_HEK293T_membrane_mock",
+    z_slice=36,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["Mock"],
+    target_database=database,
+    target_channel_name="raw Cy5 EX639 EM698-70",
+    prediction_database=database,
+    prediction_channel_name="nuclei_prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_nuclei_mock",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["DENV"],
+    target_database=database,
+    target_channel_name="raw Cy5 EX639 EM698-70",
+    prediction_database=database,
+    prediction_channel_name="nuclei_prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_nuclei_DENV",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["Mock"],
+    target_database=database,
+    target_channel_name="raw mCherry EX561 EM600-37",
+    prediction_database=database,
+    prediction_channel_name="membrane_prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_membrane_mock",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["DENV"],
+    target_database=database,
+    target_channel_name="raw mCherry EX561 EM600-37",
+    prediction_database=database,
+    prediction_channel_name="membrane_prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_membrane_DENV",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+# %% Compute CellDiff intensity metrics
 
 # Construct required databases - cropped target and CellDiff prediction are saved in target_root and pred_root
 hek_h2b_database = database[
@@ -78,8 +173,121 @@ metrics = compute_metrics(
     prediction_database=pred_database,
     prediction_channel_name="Nuclei-prediction",
     log_output_dir=output_dir,
-    log_name="intensity_CellDiff",
+    log_name="intensity_CellDiff_HEK293T_nuclei",
     z_slice=36-15,  # Crop start at slice 15
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["HEK293T"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["Mock"],
+    target_database=target_database,
+    target_channel_name="Membrane",
+    prediction_database=pred_database,
+    prediction_channel_name="Membrane-prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_CellDiff_HEK293T_membrane",
+    z_slice=36-15,  # Crop start at slice 15
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+a549_h2b_database = database[
+    (database["Organelle"] == "HIST2H2BE") & (database["Cell type"] == "A549")
+]
+pred_database = a549_h2b_database.copy()
+target_database = a549_h2b_database.copy()
+
+old_root = Path("/hpc/projects/intracellular_dashboard/organelle_dynamics/rerun/2025_04_17_A549_H2B_CAAX_DENV/2-assemble/2025_04_17_A549_H2B_CAAX_DENV.zarr")
+pred_root = Path("/hpc/projects/virtual_staining/datasets/huang-lab/prediction/a549/output.zarr")
+target_root = Path("/hpc/projects/virtual_staining/datasets/huang-lab/crops/a549/2025_04_17_A549_H2B_CAAX_DENV.zarr")
+
+pred_database["Path"] = pred_database["Path"].apply(partial(replace_root, new_root=pred_root))
+target_database["Path"] = target_database["Path"].apply(partial(replace_root, new_root=target_root))
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["Mock"],
+    target_database=target_database,
+    target_channel_name="raw Cy5 EX639 EM698-70",
+    prediction_database=pred_database,
+    prediction_channel_name="Nuclei-prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_nuclei_mock",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["Mock"],
+    target_database=target_database,
+    target_channel_name="raw mCherry EX561 EM600-37",
+    prediction_database=pred_database,
+    prediction_channel_name="Membrane-prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_membrane_mock",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["DENV"],
+    target_database=target_database,
+    target_channel_name="raw Cy5 EX639 EM698-70",
+    prediction_database=pred_database,
+    prediction_channel_name="Nuclei-prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_nuclei_DENV",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
+)
+
+metrics = compute_metrics(
+    metrics_module=IntensityMetrics(),
+    cell_types=["A549"],
+    organelles=["HIST2H2BE"],
+    infection_conditions=["DENV"],
+    target_database=target_database,
+    target_channel_name="raw mCherry EX561 EM600-37",
+    prediction_database=pred_database,
+    prediction_channel_name="Membrane-prediction",
+    log_output_dir=output_dir,
+    log_name="intensity_VSCyto3D_A549_membrane_DENV",
+    z_slice=10,
+    transforms=[
+        NormalizeIntensityd(
+            keys=["pred", "target"],
+        )
+    ],
 )
 
 # %%
