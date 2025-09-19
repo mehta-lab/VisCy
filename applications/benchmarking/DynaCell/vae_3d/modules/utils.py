efbb1883bcf3045461340352514fcfcb757bf202
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2025-09-18 18:36:42 -0700

    deleting uncessary files. keeping only the torch script related scripts

--- applications/benchmarking/DynaCell/vae_3d/modules/utils.py
+++
@@ -1,9 +0,0 @@
-import torch
-from dataclasses import dataclass
-from transformers.utils import ModelOutput
-
-@dataclass
-class VAEOutput(ModelOutput):
-    loss: torch.FloatTensor = None
-    recon_loss: torch.FloatTensor = None
-    kl_loss: torch.FloatTensor = None
