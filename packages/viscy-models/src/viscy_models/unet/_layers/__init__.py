719c940e6c29ae4b1312db7698f0c39f672803ce
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2026-02-13 12:14:43 -0800

    remove the _layers

--- packages/viscy-models/src/viscy_models/unet/_layers/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Backward-compatible re-exports from _components."""
-
-from viscy_models._components.conv_block_2d import ConvBlock2D
-from viscy_models._components.conv_block_3d import ConvBlock3D
-
-__all__ = ["ConvBlock2D", "ConvBlock3D"]
