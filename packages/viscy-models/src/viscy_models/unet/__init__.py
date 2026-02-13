67859a9e237ff477499feb0c69f8aa2f194508fb
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2026-02-12 17:17:01 -0800

    feat(07-02): migrate FullyConvolutionalMAE to viscy-models
    - Copy FCMAE and all helper classes/functions to unet/fcmae.py
    - Replace old viscy imports with viscy_models._components imports
    - Remove duplicated PixelToVoxelShuffleHead (import from _components.heads)
    - Fix mutable list defaults to tuples (encoder_blocks, dims)
    - Export both UNeXt2 and FullyConvolutionalMAE from unet/__init__.py

--- packages/viscy-models/src/viscy_models/unet/__init__.py
+++ packages/viscy-models/src/viscy_models/unet/__init__.py
@@ -1,5 +1,6 @@
 """UNet family architectures."""
 
+from viscy_models.unet.fcmae import FullyConvolutionalMAE
 from viscy_models.unet.unext2 import UNeXt2
 
-__all__ = ["UNeXt2"]
+__all__ = ["UNeXt2", "FullyConvolutionalMAE"]
