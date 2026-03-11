e0950cb9d5f20184f253761f8fc25dd2210e6664
Eduardo Hirata-Miyasaki <edhiratam@gmail.com>
2026-03-11 10:59:40 -0700

    obs should use fov_name and track_id and id as UUID

--- applications/dynaclr/configs/linear_classifiers/append_obs_slurm.sh
+++ applications/dynaclr/configs/linear_classifiers/append_obs_slurm.sh
@@ -14,10 +14,10 @@ export PYTHONNOUSERSITE=1
 # --- Edit these paths --------------------------------------------------------
 WORKSPACE_DIR=/hpc/mydata/eduardo.hirata/repos/viscy
 
-EMBEDDINGS=/hpc/projects/intracellular_dashboard/organelle_dynamics/2025_07_22_A549_SEC61_TOMM20_G3BP1_ZIKV/4-phenotyping/predictions/DINOv3/convnext-tiny-lvd1689m/dinov3-convnext-tiny-phase_2d_embeddings.zarr
-CSV=/hpc/projects/organelle_phenotyping/datasets/annotations/2025_07_22_A549_SEC61_TOMM20_G3BP1_ZIKV/2025_07_22_A549_SEC61_TOMM20_G3BP1_ZIKV_combined_annotations.csv
+EMBEDDINGS_DIR=/hpc/projects/intracellular_dashboard/organelle_dynamics/2025_01_28_A549_G3BP1_ZIKV_DENV/4-phenotyping/predictions/DynaCLR-2D-BagOfChannels-timeaware/v3
+CSV=/hpc/projects/organelle_phenotyping/datasets/annotations/2025_01_28_A549_G3BP1_ZIKV_DENV/2025_01_28_A549_G3BP1_ZIKV_DENV_combined_annotations.csv
 PREFIX=annotated_
-MERGE_KEY=id
+MERGE_KEY=fov_name
 # -----------------------------------------------------------------------------
 
 scontrol show job $SLURM_JOB_ID
@@ -28,9 +28,16 @@ echo "Prefix: $PREFIX"
 echo "Merge key: $MERGE_KEY"
 echo ""
 
-uv run --project "$WORKSPACE_DIR" --package dynaclr --extra eval \
-    dynaclr append-obs \
-    -e "$EMBEDDINGS" \
-    --csv "$CSV" \
-    --prefix "$PREFIX" \
-    --merge-key "$MERGE_KEY"
+#For each zarr in the embeddings path, append the obs to the csv
+for ZARR in $EMBEDDINGS_DIR/*.zarr; do
+    echo "Appending obs to $ZARR"
+    uv run --project "$WORKSPACE_DIR" --package dynaclr --extra eval \
+        dynaclr append-obs \
+        -e "$ZARR" \
+        --csv "$CSV" \
+        --prefix "$PREFIX" \
+        --merge-key "fov_name" \
+        --merge-key "track_id" \
+        --merge-key "id"
+
+done
